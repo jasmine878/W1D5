a8f322b9daf28697200b23cd73084bd92ca31329
App Academy Student <jasmine878@yahoo.com>
2018-11-30 16:03:49 -0500

    Create parent setter method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,12 +1,36 @@
+require 'byebug'
+
 class PolyTreeNode
-    attr_reader :parent, :children
-    def initialize(value)
+    attr_reader :children, :value, :parent
+    # attr_accessor :parent
+
+    def initialize(value = "root")
         @value = value
         @parent = nil
         @children = []
     end
 
-    def parent
-        
+    def parent=(new_parent)  #node2.parent = node1
+        unless self.parent.nil?
+            self.parent.children.delete(self)
+        end
+
+        if new_parent.nil?
+            @parent = nil
+        else
+            @parent = new_parent
+            unless @parent.children.include?(self)
+                @parent.children << self
+            end
+        end
     end
+
+    def add_child(child_node)
+        @parent = child_node
+    end
+
+    def remove_child(child_node)
+
+    end
+
 end
