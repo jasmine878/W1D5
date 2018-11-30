d16f5b6260d7d157cade934238d5af5b344ae800
App Academy Student <jasmine878@yahoo.com>
2018-11-30 17:30:05 -0500

    Add dfs method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -25,12 +25,33 @@ class PolyTreeNode
         end
     end
 
-    def add_child(child_node)
-        @parent = child_node
+    def add_child(child_node) # node2.add_child(node3)
+        child_node.parent = self
     end
 
     def remove_child(child_node)
-
+        if child_node.parent.nil?
+            raise
+        else
+            child_node.parent = nil
+        end
     end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        # debugger
+
+        self.children.each do |child|
+            node = child.dfs(target_value) #node = nil for child= 4
+            #memoize recursive call bc if it's the node we're looking for it will return the node
+            return node unless node.nil?
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        
+    end
+
 
 end
